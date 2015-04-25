fedc880a6962bd4a82448cd658db4a8063dd9f20
Mateus Freira <mateus.freira@gmail.com>
2015-04-24 21:29:15 -0300

    Solving

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## Just make a object with the methods  set, get, setSolve, getSolve based on the 'x' matrix passed by parameter
+#
 makeCacheMatrix <- function(x = matrix()) {
-
+    s <- NULL
+    set <- function(y) {
+      x <<- y
+      s <<- NULL
+    }
+    get <- function() x
+    setSolve <- function(solve) s <<- solve
+    getSolve <- function() s
+    list(
+          set = set, 
+          get = get,
+          setSolve = setSolve,
+          getSolve = getSolve
+      )
 }
 
 
-## Write a short comment describing this function
-
+## try get solve value from cache and if the cache values return NULL calculate the solve and store on cache for future requests, then return the solve value
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getSolve()
+  if(is.null(m)) {
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setSolve(m) 
+  }
+  m
 }
